b13fadc6cff43aec5e8ac9d24191d3ba96d97a6d
Leonid (notebook) <leonidrawhide@gmail.com>
2022-03-17 16:06:08 +0300

    fixed material ui error

--- src/app/book/book.module.ts
+++ src/app/book/book.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { AppModule } from './app.module';
+import { AppModule } from '../app.module';
 import { PageOneComponent } from './page-one/page-one.component';
 import { PageTwoComponent } from './page-two/page-two.component';
 import { PageThreeComponent } from './page-three/page-three.component';
@@ -10,7 +10,13 @@ import { PageFiveComponent } from './page-five/page-five.component';
 
 
 @NgModule({
-  declarations: [PageOneComponent, PageTwoComponent, PageThreeComponent, PageFourComponent, PageFiveComponent],
+  declarations: [
+    PageOneComponent, 
+    PageTwoComponent, 
+    PageThreeComponent, 
+    PageFourComponent, 
+    PageFiveComponent
+  ],
   imports: [
     CommonModule,
     AppModule
